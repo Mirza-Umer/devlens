f0ec5c4f66f2a5f88aeb6035f10b87ec5e2a43d3
Mirza-Umer <mirzaumer292@gmail.com>
2026-06-16 21:38:04 +0500

    Initial NestJS project scaffold
    Add initial NestJS application skeleton including README and .gitignore; formatting and linting configs (.prettierrc, eslint.config.mjs); Nest CLI and TypeScript configs (nest-cli.json, tsconfig.json, tsconfig.build.json). Add package.json and package-lock.json plus test setup (jest e2e config and tests). Create source tree with core app files (main.ts, app.module/controller/service) and a 'projects' feature (module, controller, service, DTOs, entity) with accompanying unit tests. This provides a ready-to-use starter for development and testing.

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProjectsModule } from './projects/projects.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'POSTGRES',
+      database: 'devlens-db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ProjectsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
